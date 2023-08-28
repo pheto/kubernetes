b3d04c53aa0f92c95d895861e8760598663c6b8a
pheto <psarosi99@gmail.com>
2023-08-28 11:53:31 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+echo "<H1>".gethostname()."</H1>";
+echo "Client time: <span id='jstime' class='padding:5em;'></span><br>";
+echo "Server time: ".date('c')."<br><br>";
+foreach($_SERVER as $key => $value){
+echo "<b>".$key."</b> = ".$value;
+echo "<br>";
+}
+
+$servername = "localhost";
+$username = "dbadmin";
+$password = "Heslo123";
+$dbname = "dbtest";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully!";
+$conn->close();
+
+?>
+
+<script>
+document.getElementById("jstime").innerHTML = new Date();
+setInterval(function(){
+        document.getElementById("jstime").innerHTML = new Date();
+}, 1000);
+</script>
