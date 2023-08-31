3301998342340fd80b03908b9561bfa5c3b9aac4
pheto <psarosi99@gmail.com>
2023-08-31 09:46:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,7 @@ $dbname = "dbtest";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-//if ($conn->connect_error) {
-if ($mysqli->connect_error) {
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
