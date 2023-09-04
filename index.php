b71c0cc94dbb0f00299a7c8df6596f752da368c0
pheto <psarosi99@gmail.com>
2023-09-04 11:34:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,15 +13,15 @@ $password = "Heslo123";
 $dbname = "dbtest";
 
 // Create connection
-//$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-//if ($conn->connect_error) {
- //   die("Connection failed: " . $conn->connect_error);
-//}
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 echo "Connected successfully!";
-//$conn->close();
+$conn->close();
 
 ?>
 
